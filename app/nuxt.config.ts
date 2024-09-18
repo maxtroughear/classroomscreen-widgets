683b32a588959dce926a78441008cdb6254dff12
Max Troughear <max@maxtroughear.dev>
2024-09-18 23:10:00 +1200

    Add riddles and Tailwind mess

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2024-04-03',
-    devtools: {enabled: true},
-    css: ['~/assets/css/main.css']
+  compatibilityDate: '2024-04-03',
+  devtools: {enabled: true},
+  css: ['~/assets/css/main.css'],
+  modules: ['@nuxtjs/tailwindcss']
 })
---
+++ app/pages/riddle.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+const {data, status, error, refresh, clear} = useFetch("https://riddles-api.vercel.app/random")
+
+const answerShown = ref(false);
+const questionReady = computed(() => {
+  return status.value == 'success' && answerShown.value == false;
+});
+
+function showAnswer() {
+  answerShown.value = true;
+}
+
+function newRiddle() {
+  refresh();
+  answerShown.value = false;
+}
+
+</script>
+
+<template>
+  <ClientOnly>
+    <div>
+      <h1 :class="{hidden: answerShown || !questionReady}">{{ data.riddle }}</h1>
+      <button v-on:click="showAnswer" :class="{hidden: answerShown}"
+              class="group transition-all italic">
+        <span
+            class="bg-left-bottom bg-gradient-to-r from-black to-black bg-[length:0%_5px] bg-no-repeat group-hover:bg-[length:100%_5px] transition-all duration-500 ease-out">
+          {{ status == 'pending' ? "Loading" : "Show answer" }}
+        </span>
+      </button>
+      <h1 :class="{ hidden: !answerShown}" class="answer">{{ data.answer }}</h1>
+      <button v-on:click="newRiddle" :class="{hidden: !answerShown}"
+              class="group transition-all italic">
+        <span
+            class="bg-left-bottom bg-gradient-to-r from-black to-black bg-[length:0%_5px] bg-no-repeat group-hover:bg-[length:100%_5px] transition-all duration-500 ease-out">
+          New riddle
+        </span>
+      </button>
+    </div>
+  </ClientOnly>
+</template>
+
+<style scoped>
+.hidden {
+  display: none;
+}
+
+button {
+  font-size: calc(3vw + 3vh);
+  font-family: "Baskerville Old Face", serif;
+  font-weight: bold;
+}
+
+h1 {
+  font-family: "Baskerville Old Face", serif;
+  user-select: none;
+  -webkit-user-select: none;
+  font-size: calc(3vw + 3vh);
+  font-weight: bold;
+  flex-basis: 100%;
+  text-align: center;
+}
+
+div {
+  padding: 0;
+  margin: 0;
+  width: 100vw;
+  min-height: 100vh;
+  height: 100%;
+  background-color: beige;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+</style>
